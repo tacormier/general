2fc909f9202f3584150c40549be11c6cb383b019
tcormier <tcormier@whrc.org>
2015-06-17 17:05:31 -0400

    added stratified function and roundUp function

--- R/handy_functions_TC.R
+++ R/handy_functions_TC.R
@@ -104,16 +104,24 @@ plotWaveform <- function(waveformDF, nameHeightCol, nameCountCol, smoothFactor=0
   wave <- waveformDF
   #calculate the smooth line
   if (lineType == "smooth") {
-    smooth_vals = predict(loess(get(nameCountCol)~get(nameHeightCol),wave, span=smoothFactor),wave$height)
+    smooth_vals = predict(loess(get(nameCountCol)~get(nameHeightCol), wave, span=smoothFactor, surface="direct"),wave[[nameHeightCol]])
+    #smooth_vals = smooth.spline(wave[[nameHeightCol]],wave[[nameCountCol]], spar=.2)
     sm.line <- as.data.frame(cbind(smooth_vals, wave[[nameHeightCol]]))
+    #sm.line <- as.data.frame(cbind(smooth_vals[[2]], smooth_vals[[1]]))
     names(sm.line) <- c("counts", "height")
   
-    p <- ggplot(wave, aes(counts, height)) + ylim(ylim) + geom_point(size=1.5) + geom_path(data=sm.line, col="chartreuse4") + 
+    p <- ggplot(wave, aes(counts, height)) + ylim(ylim) + geom_point(size=0.75)  + geom_path(data=sm.line, col="chartreuse4", size=0.5) + 
       theme_bw() + theme(legend.position="none") 
+    
+    # to include points too!
+    #+ geom_point(size=1.5) 
   
     } else if (lineType == "exact") {
-      p <- ggplot(wave, aes(counts, height)) + ylim(ylim) + geom_point(size=1.5) + geom_path(data=wave, col="chartreuse4") + 
+      p <- ggplot(wave, aes(counts, height)) + ylim(ylim) + geom_path(data=wave, col="chartreuse4", size=0.5) + 
         theme_bw() + theme(legend.position="none") 
+      
+      # to include points too!
+      #+ geom_point(size=1.5) 
       
   } else {
     stop(paste0("The lineType argument must be either 'smooth' or 'exact.' You entered '", lineType, "'. Please try again."))
@@ -483,5 +491,117 @@ writeMDDB_multPixelsPerPlot <- function(inPlot, xcol, ycol,responseCol, dist.m, 
   return(results)
 } # end writeMDDB_multPixelsPerPlot function
   
-                                    
+# ################## Stratified Random Sampling ################## 
+# stratified = function(df, group, size) {
+#   #  USE: * Specify your data frame and grouping variable (as column 
+#   #         name) as the first two arguments.
+#   #       * Decide on your sample size. For a sample proportional to the
+#   #         population, enter "size" as a decimal. For an equal number 
+#   #         of samples from each group, enter "size" as a whole number.
+#   #
+#   #  Example 1: Sample 10% of each group from a data frame named "z",
+#   #             where the grouping variable column name is "nameGrpCol", use:
+#   # 
+#   #                 > stratified(z, "nameGrpCol", .1)
+#   #
+#   #  Example 2: Sample 5 observations from each group from a data frame
+#   #             named "z"; grouping variable column name is "nameGrpCol", use:
+#   #
+#   #                 > stratified(z, "nameGrpCol", 5)
+#   #
+#   require(sampling)
+#   
+#   # column # for group
+#   group <- which(names(df) %in% group, arr.ind = T)
+#   temp = df[order(df[group]),]
+#   # Calc # of samples per group
+#   if (size < 1) {
+#     nsamp = ceiling(table(temp[group]) * size)
+#   } else if (size >= 1) {
+#     nsamp = rep(size, times=length(table(temp[group])))
+#   }  
+#   # Select sample
+#   strat = strata(temp, stratanames = names(temp[group]), 
+#                  size = as.vector(nsamp), method = "srswor")
+#   (dsample = getdata(temp, strat))
+# }
+
+# This function came from : https://gist.github.com/mrdwab/6424112
+# The arguments to stratified are:
+#   
+# df: The input data.frame
+# group: A character vector of the column or columns that make up the "strata".
+# size: The desired sample size.
+# If size is a value less than 1, a proportionate sample is taken from each stratum.
+# If size is a single integer of 1 or more, that number of samples is taken from each stratum.
+# If size is a vector of integers, the specified number of samples is taken for each stratum. It is recommended that you use a named vector. 
+# For example, if you have two strata, "A" and "B", and you wanted 5 samples from "A" and 10 from "B", you would enter size = c(A = 5, B = 10).
+# select: This allows you to subset the groups in the sampling process. This is a list. For instance, if your group variable was 
+# "Group", and it contained three strata, "A", "B", and "C", but you only wanted to sample from "A" and "C", you can use select = list(Group = c("A", "C")).
+# replace: For sampling with replacement.                                    
+stratified <- function(df, group, size, select = NULL, 
+                       replace = FALSE, bothSets = FALSE) {
+  if (is.null(select)) {
+    df <- df
+  } else {
+    if (is.null(names(select))) stop("'select' must be a named list")
+    if (!all(names(select) %in% names(df)))
+      stop("Please verify your 'select' argument")
+    temp <- sapply(names(select),
+                   function(x) df[[x]] %in% select[[x]])
+    df <- df[rowSums(temp) == length(select), ]
+  }
+  df.interaction <- interaction(df[group], drop = TRUE)
+  df.table <- table(df.interaction)
+  df.split <- split(df, df.interaction)
+  if (length(size) > 1) {
+    if (length(size) != length(df.split))
+      stop("Number of groups is ", length(df.split),
+           " but number of sizes supplied is ", length(size))
+    if (is.null(names(size))) {
+      n <- setNames(size, names(df.split))
+      message(sQuote("size"), " vector entered as:\n\nsize = structure(c(",
+              paste(n, collapse = ", "), "),\n.Names = c(",
+              paste(shQuote(names(n)), collapse = ", "), ")) \n\n")
+    } else {
+      ifelse(all(names(size) %in% names(df.split)),
+             n <- size[names(df.split)],
+             stop("Named vector supplied with names ",
+                  paste(names(size), collapse = ", "),
+                  "\n but the names for the group levels are ",
+                  paste(names(df.split), collapse = ", ")))
+    }
+  } else if (size < 1) {
+    n <- round(df.table * size, digits = 0)
+  } else if (size >= 1) {
+    if (all(df.table >= size) || isTRUE(replace)) {
+      n <- setNames(rep(size, length.out = length(df.split)),
+                    names(df.split))
+    } else {
+      message(
+        "Some groups\n---",
+        paste(names(df.table[df.table < size]), collapse = ", "),
+        "---\ncontain fewer observations",
+        " than desired number of samples.\n",
+        "All observations have been returned from those groups.")
+      n <- c(sapply(df.table[df.table >= size], function(x) x = size),
+             df.table[df.table < size])
+    }
+  }
+  temp <- lapply(
+    names(df.split),
+    function(x) df.split[[x]][sample(df.table[x],
+                                     n[x], replace = replace), ])
+  set1 <- do.call("rbind", temp)
+  
+  if (isTRUE(bothSets)) {
+    set2 <- df[!rownames(df) %in% rownames(set1), ]
+    list(SET1 = set1, SET2 = set2)
+  } else {
+    set1
+  }
+}
+
+################## Round up to nearest "to" value ################## 
+roundUp <- function(x,to=10) to*(x%/%to + as.logical(x%%to))
 
