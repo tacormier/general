1abf3bd6f398c235df3c9ef5ca359f799107c47e
tcormier <tcormier@whrc.org>
2017-02-06 10:51:16 -0500

    1/23/2017 Some additional code working specifically on 2007. Comparing total of the 2007 biomass in the amazon basin with the change analysis raster from 2007.

--- R/clip_mask_biomassChg_forAle.R
+++ R/clip_mask_biomassChg_forAle.R
@@ -1,6 +1,7 @@
 library(raster)
 library(maptools)
 library(gdalUtils)
+library(ggplot2)
 rasterOptions(tmpdir="/home/tcormier/RasTmpDir/")
 gdal_setInstallation(search_path="/mnt/s/gdal-1.10.1/bin/",rescan=T)
 
@@ -124,6 +125,158 @@ br <- stack(st.files)
 writeRaster(br, paste0(outdir, "final/Biomass_2003-2014.tif"), datatype="INT2U", overwrite=T)
 
 
+####################
+# 1/23/2017 Some additional code working specifically on 2007.
+# Wayne asked for the total of the 2007 biomass in the amazon basin from the change analysis
+# AND the 2007 total from the original pan-tropical biomass map ca. 2007.
+library(ggplot2)
+library(hexbin)
 
 
+yr=8
+biodir <- "/mnt/a/change/modis/america/luis/annualbio/"
+boundfile <- "/mnt/a/RAISG/RAISGlimits_sin.shp"
+outdir <- "/mnt/a/tcormier/for_Ale/bio_mask_clip/check_annVsOrigPantrop/"
+dir.create(outdir)
+
+chg.2007 <- raster("/mnt/a/tcormier/for_Ale/bio_mask_clip/mosaics_masked_clipped/de2007_mc.tif")
+chg.2007.all <- raster("/mnt/a/tcormier/for_Ale/bio_mask_clip/mosaics/de2007.vrt")
+orig.2007 <- raster("/mnt/a/biov2/america/am_biov2ct1.tif")
+
+b <- readShapePoly(boundfile)
+
+# crop orig to chg
+orig.crop <- crop(orig.2007, b)
+# Line up the pixels (tried to do this in parallel, but it was VERY messed up)
+orig.resamp <- resample(orig.crop, chg.2007, method='ngb')
+
+orig.resamp[is.na(chg.2007)] <- NA
+chg.2007[is.na(orig.resamp)] <- NA
+
+# Let's write these out to files JUST to be sure they are masked properly (they are).
+# identical(is.na(orig.resamp), is.na(chg.2007)) # This was FALSE - but true as a vector
+writeRaster(orig.resamp, paste0(outdir, "orig_2007_mc.tif"), datatype=dataType(orig.2007), overwrite=T)
+writeRaster(chg.2007, paste0(outdir, "chgAnalysis_2007_mc.tif"), datatype=dataType(chg.2007), overwrite=T)
+
+orig.vec <- as.vector(orig.resamp)
+chg.vec <- as.vector(chg.2007)
+
+ona <- is.na(orig.vec)
+cna <- is.na(chg.vec)
+identical(ona, cna)
+
+# Sum in total tons (so multiply sum by area of a pixel = 463.3127^2)
+summary(orig.vec)
+summary(chg.vec)
+
+orig.vec <- orig.vec[!is.na(orig.vec)]
+chg.vec <- chg.vec[!is.na(chg.vec)]
+
+# Sum in the basin = MG/ha
+orig.sum <- sum(orig.vec)
+chg.sum <- sum(chg.vec)
+
+# Try it one more way
+stats.orig <- cellStats(orig.resamp, stat='sum', na.rm=T)
+stats.chg <- cellStats(chg.2007, stat='sum', na.rm=T)
+
+# In millions of metric tons of Carbon. /area of a pixel by 10000 to get to ha, then by a million to get millions
+# of metric tons of biomass, then by 2 to get millions of metric tons of Carbon
+orig.totbio <- (orig.sum*((463.3127^2)/10000))/1000000/2
+chg.totbio <- (chg.sum*((463.3127^2)/10000))/1000000/2
+
+################################
+# Do the same thing with the whole basin - not just change
+chg.all <- crop(chg.2007.all, b)
+# m <- mask(m1, b)
+beginCluster()
+chg.mask <- clusterR(chg.all, mask, args=list(mask=b))
+endCluster()
+
+# line up pixels
+orig.resamp2 <- resample(orig.crop, chg.mask, method='ngb')
+orig.resamp2[is.na(chg.mask)] <- NA
+chg.mask[is.na(orig.resamp2)] <- NA
+
+#
+orig.vec2 <- as.vector(orig.resamp2)
+chg.vec2 <- as.vector(chg.mask)
+
+ona2 <- is.na(orig.vec2)
+cna2 <- is.na(chg.vec2)
+identical(ona2, cna2)
+
+orig.vec2 <- orig.vec2[!is.na(orig.vec2)]
+chg.vec2 <- chg.vec2[!is.na(chg.vec2)]
+
+# Sum in the basin = MG/ha
+orig.sum2 <- sum(orig.vec2)
+chg.sum2 <- sum(chg.vec2)
+
+
+# In millions of metric tons of Carbon. /area of a pixel by 10000 to get to ha, then by a million to get millions
+# of metric tons of biomass, then by 2 to get millions of metric tons of Carbon
+orig.totbio2 <- (orig.sum2*((463.3127^2)/10000))/1000000/2
+chg.totbio2 <- (chg.sum2*((463.3127^2)/10000))/1000000/2
+
+# Try it one more way - all checks out!
+stats.orig2 <- cellStats(orig.resamp2, stat='sum', na.rm=T)
+stats.chg2 <- cellStats(chg.mask, stat='sum', na.rm=T)
+stats.orig.totbio2 <- (stats.orig2*((463.3127^2)/10000))/1000000/2
+stats.chg.totbio2 <- (stats.chg2*((463.3127^2)/10000))/1000000/2
+
+df <- cbind.data.frame(orig.vec2, chg.vec2)
+names(df) <- c("original", "changeAnalysis")
+
+# bin<-hexbin(orig.vec2, chg.vec2, xbins=50) 
+# plot(bin, main="Hexagonal Binning", xlim=c(0,400))
+
+hb <- ggplot(df, aes(original, changeAnalysis)) + xlim(0,425) + ylim(0,425)
+hb + stat_binhex(aes(colour = ..value..)) + geom_abline(intercept = 0, color="red")
+
+# Take a thousand random samples
+s <- sample(nrow(df), 1000)
+df.samp <- df[s,]
+p <- ggplot(df.samp, aes(original, changeAnalysis)) + xlim(0,375) + ylim(0,375) + geom_point() + geom_abline(intercept = 0, color="red")
+p
+
+###############
+# Ok, so issues. Now we are going to the raw data. 
+# Instructions from Ale: could you please take a random sample 
+# from band 11 in  /mnt/a/change/modis/america/inputs2/b.2007.h11v09.bip  
+# and plot it versus band 11  in /mnt/a/mod68b/h11v09.bip
+
+chg.raw <- as.vector(raster("/mnt/a/change/modis/america/inputs2/b.2007.h11v09.bip", band=11))
+orig.raw <- as.vector(raster("/mnt/a/mod68b/h11v09.bip", band=11))
+chg.raw2 <- as.vector(raster("/mnt/a/change/modis/america/inputs2/b.2007.h12v10.bip", band=11))
+orig.raw2 <- as.vector(raster("/mnt/a/mod68b/h12v10.bip", band=11))
+
+s <- sample(length(chg.raw), 5000)
+
+chg.r.s <- chg.raw[s]
+orig.r.s <- orig.raw[s]
+df.raw <- cbind.data.frame(orig.r.s, chg.r.s)
+names(df.raw) <- c("raw_original", "raw_change")
+
+p <- ggplot(df.raw, aes(raw_original, raw_change)) + geom_point() + geom_abline(intercept = 0, color="red")
+p
+
+# Now for the second tile
+s2 <- sample(length(chg.raw2), 5000)
+
+chg.r.s2 <- chg.raw[s2]
+orig.r.s2 <- orig.raw[s2]
+df.raw2 <- cbind.data.frame(orig.r.s2, chg.r.s2)
+names(df.raw2) <- c("raw_original", "raw_change")
+
+p2 <- ggplot(df.raw2, aes(raw_original, raw_change)) + geom_point() + geom_abline(intercept = 0, color="red")
+p2
+
+# And all together now...
+df.raw3 <- rbind.data.frame(df.raw, df.raw2)
+p3 <- ggplot(df.raw3, aes(raw_original, raw_change)) + geom_point() + geom_abline(intercept = 0, color="red")
+p3
+
+lm <- lm(df.raw3$raw_change ~ df.raw3$raw_original)
+summary(lm) 
 
