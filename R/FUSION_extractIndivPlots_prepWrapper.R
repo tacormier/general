8f828db2e923d6bc6d7d37b9cabf31492b56b3e6
tcormier <tcormier@whrc.org>
2017-02-16 11:43:02 -0500

    made updates to file naming to help identify duplicate plot extractions

--- R/FUSION_extractIndivPlots_prepWrapper.R
+++ R/FUSION_extractIndivPlots_prepWrapper.R
@@ -1,7 +1,7 @@
 # This script calls the /mnt/a/tcormier/scripts/general/R/FUSION_extractPlots.R and submits each plot 
 # from each polygon file in paramfile to the cluster as a separate job.
 #
-library(rgdal)
+# library(rgdal)
 library(raster)
 library(rgeos)
 library(maptools)
@@ -13,18 +13,19 @@ Sys.setenv(SGE_ROOT = "/net/share-2/export/HomeDir/sge6.2/")
 Sys.setenv(SGE_CELL="Grid-Cell-01")
 
 ############# Variables #################
-# csv file containing parameters for each run. Field names must be polyPath, ID_field_num, and lasindex file (shp). 
+# csv file containing parameters for each run. Field names must be polyPath, ID_field_name, and lasindex file (shp). 
 # So this table is a list of shapefiles and the accompanying las indices.
-
-# Example file: /mnt/a/tcormier/Mexico_CMS/lidar/field_intersect/las_extract/FUSION_extractPlots_params/extract_infys_20150918.csv
+# Example file: 
+# paramfile <- "/mnt/a/tcormier/Mexico_CMS/lidar/field_intersect/las_extract/FUSION_extractPlots_params/extract_infys_20150918.csv"
 # paramfile <- "/mnt/a/tcormier/Mexico_CMS/lidar/G-LiHT/field_lidar_intersect/FUSION_extractPlots_params_20151110/G-LiHT_params_20151110.csv"
-paramfile <- "/mnt/a/tcormier/Mexico_CMS/lidar/field_intersect/las_extract/FUSION_extractPlots_params/extract_workshopArea_20160401.csv"
+# paramfile <- "/mnt/a/tcormier/Mexico_CMS/lidar/field_intersect/las_extract/FUSION_extractPlots_params/extract_workshopArea_20160401.csv"
+paramfile <- "/mnt/a/tcormier/Mexico_CMS/lidar/field_intersect/las_extract/FUSION_extractPlots_params/extract_CMS_allPlots_20170215.csv"
 # 
 # Minimum area of lidar coverage within plot (as a percentage)
 min.area <- 95
 
 # outdir <- "/mnt/a/tcormier/Mexico_CMS/lidar/G-LiHT/field_lidar_intersect/" Does not need to exist.
-outdir <- "/mnt/a/tcormier/Mexico_CMS/workshops/Mexico_lidar_201604/lidar/SampleData/"
+outdir <- "/mnt/a/tcormier/Mexico_CMS/lidar/field_intersect/all_20170215/"
 
 # Do the laslists need to be reformated to run windows tool with wine? (see function documentation)? Y or N?
 reform <- 'Y'
@@ -36,14 +37,22 @@ deltmp <- 'N'
 QLOG <- "/mnt/a/tcormier/scripts/logs/CMS_lidarExtract/"
 
 ##########################################
+# Make some directories
 dir.create(QLOG, showWarnings = F)
+dir.create(outdir, showWarnings = F)
+# Directory for incomplete plots
+incomp.dir <- paste0(outdir, "/incomplete_plots/")
+dir.create(incomp.dir, showWarnings = F)
+# Directory for complete plots
+comp.dir <- paste0(outdir, "/complete_plots/")
+dir.create(comp.dir, showWarnings = F)
 
 # open param file, do some formatting and set up loop
 params <- read.csv(paramfile, stringsAsFactors = F)
 params$outBase <- paste0(outdir, basename(params$lasindex))
 # params$outBase <- paste0(outdir, basename(params$polyPath))
 params$outBase <- gsub(".shp", ".las", params$outBase)
-dir.create(outdir)
+
 
 # We had some memory issues with submitting groups of plots to polyClipData, so let's
 # create a temp directory and write out individual shapefiles for each plot, then 
@@ -52,23 +61,27 @@ dir.create(outdir)
 
 # Create a temp dir to store individual plots
 tmpdir <- paste0(outdir, "tmp_indivPlots/")
-dir.create(tmpdir)
+dir.create(tmpdir, showWarnings=F)
 
 # Loop over each shapefile
 for (p in (1:length(params$polyPath))) {
   # strip ".shp" from paths
-  li <- unlist(str_split(params$lasindex[p], "\\."))[1]
-  ps <- unlist(str_split(params$polyPath[p], "\\."))[1]
-  id.col <- unlist(str_split(params$ID_field_num[p], "\\."))[1]
+  li <- params$lasindex[p]
+  ps <- params$polyPath[p]
+  colname.id <- params$ID_Field_Name[p]
+  
   # open the two shapefiles
-  lasindex <- readOGR(dirname(li), basename(li))
-  plots.all <- readOGR(dirname(ps), basename(ps))
-  colname.id <- colnames(plots.all@data)[as.numeric(id.col)]
+  lasindex <- shapefile(li)
+  plots.all <- shapefile(ps)
+  id.col <- which(colnames(plots.all@data) %in% colname.id) 
   
   # We only want to work with plots that intersect the lasfiles we have in the index, not
   # the whole shapefile of potentially thousands of plots.
   # plotsInLas <- over(lasindex, plots.all)
   plotsInLas <- raster::intersect(lasindex, plots.all)
+  
+  # If there are no plots that intersect, go to the next one
+  if (is.null(plotsInLas))  next()
   
   # If plots are on the edges of lastiles (i.e. the entire plot area is not fully covered by lidar data, whether by one lastile or with multiple),
   # we need to exclude them. So, first dissolve polygons with the same ID (plots in las - dissolved = pid).
@@ -100,24 +113,27 @@ for (p in (1:length(params$polyPath))) {
   # ID incompletely covered plots (% coverage is less than min.area)
   areaInt$perc_cov <- round((areaInt$area/areaInt$orig_area)*100,2)
   
-  # Now remove plots < min.area - LEFT OFF HERE 20160129
+  # Now remove plots < min.area 
   plots.complete <- plots[plots@data[[colname.id]] %in% areaInt$pid_ID[areaInt$perc_cov >= min.area],]
  
   # ****Also write shapefile of excluded plots - for manual checking that everything is going as expected.
   inc.pos <- !(plots@data[[colname.id]] %in% areaInt$pid_ID[areaInt$perc_cov >= min.area])
   if (TRUE %in% inc.pos) {
     plots.incomplete <- plots[!(plots@data[[colname.id]] %in% areaInt$pid_ID[areaInt$perc_cov >= min.area]),]
-    writeOGR(plots.incomplete, dsn = "/mnt/a/tcormier/testing/incomplete_plots/", layer = paste0("incomplete_plots_", basename(li)), driver = "ESRI Shapefile", overwrite_layer = T)
+    shapefile(plots.incomplete, paste0(incomp.dir, "incomplete_plots_", basename(li)), overwrite = T)
   }
-  writeOGR(plots.complete, dsn = "/mnt/a/tcormier/testing/complete_plots/", layer = paste0("complete_plots_", basename(li)) , driver = "ESRI Shapefile", overwrite_layer = T)
+  
+  if (nrow(plots.complete) == 0) next()
+    
+    shapefile(plots.complete, paste0(comp.dir, "complete_plots_", basename(li)) , overwrite = T)
   
   # loop over each plot
   for (pl in (1:nrow(plots.complete))) {
     p.indiv <- plots.complete[pl,]
     #id <- as.character(p.indiv@data[[colname.id]])
-    id <- as.character(p.indiv@data[,params$ID_field_num[p]])
-    newfile <- paste0(tmpdir,basename(ps), "_", id, ".shp")
-    newoutbase <- paste0(outdir, basename(li), "_", id, ".las")
+    id <- as.character(p.indiv@data[,params$ID_Field_Name[p]])
+    newfile <- paste0(tmpdir, "TCID_", id, "_", stripExtBase(ps), "_", stripExtBase(li),".shp")
+    newoutbase <- paste0(outdir, "TCID_", id, "_", stripExtBase(ps), "_", stripExtBase(li), ".las")
     
     # now intersect plot with las index to see which lasfiles we need
     intras <- raster::intersect(lasindex, p.indiv)
@@ -126,22 +142,24 @@ for (p in (1:length(params$polyPath))) {
     # plots with the lasindex)
     if (is.null(intras)) next()
     
-    writeOGR(p.indiv, tmpdir, paste0(basename(ps), "_", id), driver="ESRI Shapefile", check_exists = T, overwrite_layer = T)
-    
+    p.indiv.out <- paste0(tmpdir, "TCID_", id, "_", stripExtBase(ps), "_", stripExtBase(li), ".shp")
+    shapefile(p.indiv, p.indiv.out, overwrite=T)
     # Now write list of intersection las files to new laslist
     laslist <- intras@data$location
-    outlist <- paste0(tmpdir, basename(ps), "_", id, "_lasIntersect.txt")
+    # HACK - fix double //
+    laslist <- gsub("//", "/", laslist)
+    outlist <- paste0(tmpdir, "TCID_", id, "_", stripExtBase(ps), "_", stripExtBase(li), "_lasIntersect.txt")
     write.table(laslist, outlist, row.names=F, col.names=F, quote=F)
     
     # Finally, write new param file into tmpdir for just this indiv plot
-    param.indiv <- data.frame(polyPath=newfile, ID_field_num=params$ID_field_num[p], lasindex=outlist, outBase=newoutbase, stringsAsFactors = F)
-    pi.name <- paste0(tmpdir, unlist(str_split(basename(paramfile), "\\."))[1], "_", id, ".csv")
+    param.indiv <- data.frame(polyPath=newfile, ID_field_num=id.col, lasindex=outlist, outBase=newoutbase, stringsAsFactors = F)
+    pi.name <- paste0(tmpdir, "TCID_", id, "_", stripExtBase(paramfile), "_", stripExtBase(li), "_params.csv")
     write.table(param.indiv, pi.name, sep=",", quote=F, row.names=F)
     
     # Reformat table for windows tool?
     if (reform == "Y") {
-      ll <- scan(param.indiv$lasindex,what="character")
-      outname <- paste0(unlist(strsplit(param.indiv$lasindex, "\\."))[1], "_wine.txt")
+      ll <- scan(param.indiv$lasindex, what="character")
+      outname <- paste0(tmpdir, stripExtBase(param.indiv$lasindex), "_wine.txt")
       # now replace original name with name of reformatted file
       param.indiv$lasindex <- outname
       
@@ -153,7 +171,7 @@ for (p in (1:length(params$polyPath))) {
     }# end reform if
     
     # Submit job to cluster
-    jobname <- paste0(unlist(strsplit(basename(newoutbase), "\\."))[1], "_", id)
+    jobname <- stripExtBase(newoutbase)
     sys.call <- paste("/net/share-2/export/HomeDir/sge6.2/bin/lx24-amd64/qsub -b yes -q nondev.q -V -N", jobname, "-l rdisk=2G", "-o",QLOG, "-e", QLOG, 
                       "/mnt/s/bin/jqsub 'R --vanilla --slave < /mnt/a/tcormier/scripts/general/R/FUSION_extractPlots.R' --args", 
                       param.indiv$polyPath[1], param.indiv$ID_field_num[1], param.indiv$lasindex[1], param.indiv$outBase[1])
@@ -165,8 +183,8 @@ for (p in (1:length(params$polyPath))) {
     # only one row, my results are transposed?? To fix, transpose it back - but use this with CAUTION!
 #     param.indiv2 <- data.frame(t(apply(param.indiv, 2, function(x) gsub("/", "\\\\\\\\", x))), stringsAsFactors = F)
 #     FUSION_polyclipdata(polyPath=param.indiv2$polyPath[1], fieldNum=param.indiv2$ID_field_num[1], outBase=param.indiv2$outBase[1], lasList=param.indiv2$lasindex[1])
-
-  } # end plot loop
+    
+    }# end plot loop
 } # end poly loop
 
 if (deltmp == "Y") {
