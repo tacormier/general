05e4bdeb91c721331ba0ea43391882ad647e2db0
tcormier <tcormier@whrc.org>
2017-02-23 08:04:48 -0500

    Big update here. Rewrote this script to accept a single argument from a wrapper that submits jobs to the cluster by site. The single argument is an rdata file containing all the variables necessary to run the metrics. Also, based on conversations with Fabio on 2/17/17 via email and skype, we updated some of the quality flags. Changed normalized height to remove 1% off the top rather than values over a hard number, such as 60m. More conservative. Also, instead of skipping entire tiles if more the 1% of the points are above the cut-off, we changed it to 2%. Added some flags for if the user wants to save the profiles and/or plot them.

--- R/process_tiles_lidarMetrics.R
+++ R/process_tiles_lidarMetrics.R
@@ -14,57 +14,36 @@ rasterOptions(tmpdir="/home/tcormier/RasTmpDir/")
 source("/mnt/a/tcormier/scripts/general/R/handy_functions_TC.R")
 
 #######################################################################
-#Variables
-indir <- "/mnt/a/tcormier/Mexico_CMS/lidar/field_intersect/las_extract/extract_20150918/txt/"
-#indir <- "/mnt/a/tcormier/SE_Asia/Ellis_Paper/lidar/Sumalindo_west/Tiles_20m/"
-#indir <- "/mnt/a/tcormier/testing/lidar_processing/windows/Tiles_20m/"
-profdir <- paste0(indir, "/profiles/")
-dir.create(profdir, showWarnings=F)
-tilesize <- 30 #in meters
-binsize <- 0.5 #vertical resolution of profile (m)
-min_ptden <- 8 #min CartoData = 8 ppm2; min SL Brazil = 4 ppm2
-vegflag <- "F" #(T/F): vegetation returns classified?
-#Values for flagging bad points or tiles - based on field data or expert knowledge
-maxht <- 80 #maximum expected tree height. If less than 1% of points > maxht, these points will be simply removed (outliers). If more than 1% of points > maxht, tile will be flagged as bad and will not be processed
-minht <- -3 #if less than 1% of points < minht, these points will be simply removed (outliers). If more than 1% of points < minht, tile will be flagged as bad and will not be processed. We expect some negative heights after normalization. Too many negative heights indicate an issue with the DTM.
-minhtamp <- 2 #min normalized height amplitude for tile to be processed. If minhtamp < (max(lasdata$z)-min(lasdata$z)) after removal of outliers, tile will not be processed (no trees)
-#do you want to save the profile so you don't have to calc again?
-#Usually yes for plot profiles. Will save to indir/profiles
-saveprof <- "Y"
+# rdata file for testing
+vars <- "/mnt/r/Mex_Lidar/G_LiHT/Hildalgo_May2013_bak/lidar/las/Tiles_30m_metrics_vars.RDATA"
+# Variables
+# Get command line arguments 
+Args <- commandArgs(trailingOnly=TRUE)
+print(Args)
 
-#regions
-regions <- c("Campeche_Yucatan", "Chiapas", "Chihuahua", "Jalisco", "Oaxaca1", "Oaxaca2", "Oaxaca3")
-#dtm.file <- "/mnt/a/fgoncalves/Mexico/dtm_mosaic.tif"
-#dtm.file <- "/mnt/a/tcormier/SE_Asia/Ellis_Paper/lidar/DTM_all/DTM_Ellis/se_asia_dtm.vrt"
-#dtm.file <- "/Volumes/share-2/fgoncalves/Mexico/dtm_mosaic.tif"
-dtmlist <-"/mnt/a/tcormier/Mexico_CMS/lidar/DTMs/lists/cartodata_dtm_vrt_list.txt"
+vars <- Args[1]
+load(vars)
 #######################################################################
-dtms <- scan(dtmlist, what="character")
-# as long as "regions" are listed in the same order as the dtms in dtmlist, this 
-# will work - otherwise, need to do some matching based on region - keeping it
-# simple for the moment!
-dtminfo <- data.frame(region=regions, dtm=dtms, stringsAsFactors = F)
-
-#dtm <- raster(dtm.file)
+dtm <- raster(dtm.file)
 
 #get filenames and coordinates (lower-left corner of the tiles)
 lasfiles <- list.files(indir, pattern="\\.txt", full.names=T)
 dummy <- sub(".txt", "", basename(lasfiles))
-#dummy <- basename(dummy)
-# dummy <- lapply(strsplit(dummy, "_"), rev)
-# xcor <- as.numeric(sapply(dummy, "[", 2))
-# ycor <- as.numeric(sapply(dummy, "[", 1))
+dummy <- basename(dummy)
+dummy <- lapply(strsplit(dummy, "_"), rev)
+xcor <- as.numeric(sapply(dummy, "[", 2))
+ycor <- as.numeric(sapply(dummy, "[", 1))
 # plot(xcor,ycor)
 
-#process each tile
+# process each tile
 #if xcor and ycor are not in file names
-xcor <- vector()
-ycor <- vector()
+# xcor <- vector()
+# ycor <- vector()
   
 #define metrics, etc
 dummyn1 <- paste0("ptden_lt", min_ptden)
-dummyn2 <- paste0("fgt_", maxht, "m_gt1")
-dummyn3 <- paste0("fgt_", maxht, "m")
+dummyn2 <- paste0("fgt_gt1")
+dummyn3 <- paste0("fgt")
 dummyn4 <- paste0("flt_minus", abs(minht), "m_gt1")
 dummyn5 <- paste0("flt_minus", abs(minht), "m")
 dummyn6 <- paste0("htamp_lt_", minhtamp, "m")
@@ -81,24 +60,17 @@ trad <- array(NA, c(length(lasfiles), 10))
   colnames(trad) <- c("meanh", "stdevh", "h25", "h50", "h75", "mch", "npeaks", "ent", "fslope", "g2t")
 
 #initiate pdf to save profile figs
-pdf(file=paste0(profdir, '/profiles.pdf'), width=7, height=7)
+if (saveWave == 'Y') pdf(file=paste0(profdir, '/profiles.pdf'), width=7, height=7)
 
+p1 <- proc.time()
 #loop
 for (p in 1:length(lasfiles)) {
   print(paste0("calculating metrics for file ", p, " of ", length(lasfiles)))
-  # big cluster to sort out which dtm (all different UTM zones, so not mosaicked - 
-  # solve this later??)
-  
-  # lasdata file - pull out region -----> can this be done outside the loop? No need to reopen the DTM for every lasfile in the same site
-  lf <- basename(lasfiles[p])
-  lf.region <- unlist(strsplit(lf, "_"))[1]
-  dtm.region <- dtminfo$dtm[pmatch(lf.region,dtminfo$region)]
-  dtm <- raster(dtm.region)
-  
+
   lasdata <- read.csv(lasfiles[p], header=F, col.names=c("x","y","z","i","a","n","r","c"))
   # This xcor/ycor calc is only if that info is not in the lasfile file name
-  xcor[p] <- min(lasdata$x)
-  ycor[p] <- min(lasdata$y)
+  # xcor[p] <- min(lasdata$x)
+  # ycor[p] <- min(lasdata$y)
 
   #if lasdata is empty, jump to next
   if (nrow(lasdata)==0) {
@@ -119,22 +91,30 @@ for (p in 1:length(lasfiles)) {
   }
   lasdata$z <- lasdata$z - ptground
   
+  #if lasdata is empty, jump to next (TC added this again here based on some G-LiHT errors)
+  if (nrow(lasdata)==0) {
+    log[p,11] <- 1
+    next()
+  }
+  
   #tile quality check
   if (!(2 %in% lasdata$c)) {log[p,1] <- 1} #No ground return
     dummy <- c(3,4,5) %in% lasdata$c
   if (vegflag == "T") {
     if (sum(dummy+0) == 0) {log[p,2] <- 1} #No vegetation return
   }
+  
+  maxht <- quantile(lasdata$z, c(0.99))
   fgt_maxht <- length(lasdata$z[lasdata$z > maxht]) / length(lasdata$z) * 100
-  if (fgt_maxht > 1) {log[p,5] <- 1; log[p,6] <- fgt_maxht} #More than 1% of normalized heights > maxht m
+  if (fgt_maxht > 2) {log[p,5] <- 1; log[p,6] <- fgt_maxht} #More than 2% of normalized heights > maxht m
   flt_minht <- length(lasdata$z[lasdata$z < minht]) / length(lasdata$z) * 100
-  if (flt_minht > 1) {log[p,7] <- 1; log[p,8] <- flt_minht} #More than 1% of normalized heights < minht m
+  if (flt_minht > 2) {log[p,7] <- 1; log[p,8] <- flt_minht} #More than 2% of normalized heights < minht m
   
   #get point density
   ptden[p] <- nrow(lasdata)/(tilesize^2)
   if (ptden[p] < min_ptden) {log[p,3] <- 1; log[p,4] <- ptden[p]}
   
-  #move to next tile if there is no ground or vegetation return, or if more than 1% of the normalized heights are outliers
+  #move to next tile if there is no ground or vegetation return, or if more than 2% of the normalized heights are outliers
   if (sum(log[p, c(1,2,5,7)]) >= 1) next()
   		
   #remove points considered outliers in tiles that passed the 1st quality check
@@ -173,16 +153,18 @@ for (p in 1:length(lasfiles)) {
     write.csv(profile, file=profname, quote = F, row.names=F)
   }
   
-  #plot full profile in pdf ---------------WHY PLOTTING FULL PROFILE INSTEAD OF TRIMMED?
-  if (p %in% seq(1,length(lasfiles),4)) par(mfrow=c(2,2), oma=c(2.5,2.5,.5,.5), mar=c(1,1,.5,.5), mgp=c(1.5, .3, 0), tcl=-0.3) #c(bottom, left, top, right)
-  plot(profile$counts, profile$height, xlab="", ylab="", type="n")
-  abline(h=profile$height[gtopid], lty=1, col="gray80")
-  abline(h=profile$height[peakid], lty=2, col="red")
-  lines(profile$counts, profile$height, col="gray30")
-  legend("topright", lasfiles[p], bty="n") 
-  if (p %in% seq(1,length(lasfiles),4)) {
-	mtext("Counts", 1, outer=T, line=1.2)
-	mtext("Height (m)", 2, outer=T, line=1.2)
+  #plot full profile in pdf
+  if (saveWave == 'Y') {
+    if (p %in% seq(1,length(lasfiles),4)) par(mfrow=c(2,2), oma=c(2.5,2.5,.5,.5), mar=c(1,1,.5,.5), mgp=c(1.5, .3, 0), tcl=-0.3) #c(bottom, left, top, right)
+    plot(profile$counts, profile$height, xlab="", ylab="", type="n")
+    abline(h=profile$height[gtopid], lty=1, col="gray80")
+    abline(h=profile$height[peakid], lty=2, col="red")
+    lines(profile$counts, profile$height, col="gray30")
+    legend("topright", basename(lasfiles[p]), bty="n") 
+    if (p %in% seq(1,length(lasfiles),4)) {
+    	mtext("Counts", 1, outer=T, line=1.2)
+    	mtext("Height (m)", 2, outer=T, line=1.2)
+    }
   }
   
   #get ground and canopy counts before trimming the profile
@@ -229,23 +211,27 @@ for (p in 1:length(lasfiles)) {
           #
   			ap1 <- prof_a[seq(1, length(prof_a), 2)]
   			ap2 <- prof_a[seq(2, length(prof_a), 2)]
-			if (length(ap1) == length(ap2)) ent_a <- ap1 + ap2 else ent_a <- ap1 + c(ap2,0)
-			ent_a[ent_a == 0] <- 0.000001
-			trad[p,8] <- -sum((ent_a/sum(ent_a)) * log(ent_a/sum(ent_a))) #ent
-        #
-			dummyid <- which(prof_a == max(prof_a[prof_h > gmax0c]))
-			if (length(dummyid) > 1) dummyid <- dummyid[length(dummyid)]
-			oc_relief <- max(prof_h) - prof_h[dummyid]
-			trad[p,9] <- max(prof_a[prof_h > gmax0c]) / oc_relief #fslope
-        #
-			trad[p,10] <- genergy/tenergy #g2t
-	}
+    		if (length(ap1) == length(ap2)) ent_a <- ap1 + ap2 else ent_a <- ap1 + c(ap2,0)
+    		ent_a[ent_a == 0] <- 0.000001
+    		trad[p,8] <- -sum((ent_a/sum(ent_a)) * log(ent_a/sum(ent_a))) #ent
+          #
+    		dummyid <- which(prof_a == max(prof_a[prof_h > gmax0c]))
+    		if (length(dummyid) > 1) dummyid <- dummyid[length(dummyid)]
+    		oc_relief <- max(prof_h) - prof_h[dummyid]
+    		trad[p,9] <- max(prof_a[prof_h > gmax0c]) / oc_relief #fslope
+          #
+    		trad[p,10] <- genergy/tenergy #g2t
+      }
 }
+  p2 <- proc.time()
+  print(p2-p1)
 
+p3 <- proc.time()
 #close pdf
-dev.off()
+if (saveWave == 'Y') dev.off()
 
 #unwrap phase
+
 pha_unw <- as.data.frame(pha/NA)
 for (pp in 1:nrow(pha)) {
   pha_unw[pp,] <- unwrap(as.numeric(pha[pp,]))
@@ -259,9 +245,15 @@ amp <- amp[,freqid]
 colnames(pha_unw) <- sub("0.", ".", paste0("pha", format(freq30, digits=2)))
 colnames(amp) <- sub("0.", ".", paste0("amp", format(freq30, digits=2)))
 
+p4 <- proc.time()
+print(p4 - p3)
+
+
 #save results
 setwd(indir)
 save(lasfiles, xcor, ycor, ptden, trad, amp, pha_unw, log, file="R_project.RData")
 write.csv(cbind(lasfiles, xcor, ycor, ptden, trad, amp, pha_unw), file="metrics.csv", row.names=F)
 write.csv(cbind(lasfiles, log), file="log.csv", row.names=F)
+
+
 
